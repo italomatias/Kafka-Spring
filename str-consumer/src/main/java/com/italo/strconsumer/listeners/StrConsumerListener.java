480f6885b0cd94d83175c60a81569cdc064148c4
Italo Reis <italomatiasdosreis@gmail.com>
2024-07-16 22:25:37 -0300

    Definido partição especifica para KAFKA consumer

--- str-consumer/src/main/java/com/italo/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/italo/strconsumer/listeners/StrConsumerListener.java
@@ -2,18 +2,26 @@ package com.italo.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1",topics = "str-topic",containerFactory = "strConsumerFactory" )
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+            @TopicPartition(topic = "str-topic",partitions = {"0"})
+            }
+            ,containerFactory = "strConsumerFactory" )
     public void listener(String message){
         log.info("LISTENER RECEIVED MESSAGE: {}." , message);
     }
 
-    @KafkaListener(groupId = "group-1",topics = "str-topic",containerFactory = "strConsumerFactory" )
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+            @TopicPartition(topic = "str-topic",partitions = {"1"})},
+            containerFactory = "strConsumerFactory" )
     public void listener2(String message){
         log.info("LISTENER 2 RECEIVED MESSAGE: {}." , message);
     }
