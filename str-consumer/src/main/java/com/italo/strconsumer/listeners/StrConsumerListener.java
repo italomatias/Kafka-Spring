3ad6dccfec02bf530f448d8ba67fdf85b6f967c3
Italo Reis <italomatiasdosreis@gmail.com>
2024-07-17 22:36:22 -0300

    Criado anotação custom para padronizar os atributos do consumo do KafkaListener e alterado listener para conumir a anotação customizada a nivel de métodos

--- str-consumer/src/main/java/com/italo/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/italo/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.italo.strconsumer.listeners;
 
+import com.italo.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -9,24 +10,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-            @TopicPartition(topic = "str-topic",partitions = {"0"})
-            }
-            ,containerFactory = "strConsumerFactory" )
+    @StrConsumerCustomListener(groupId = "group-1")
     public void listener(String message){
         log.info("LISTENER RECEIVED MESSAGE: {}." , message);
     }
 
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-            @TopicPartition(topic = "str-topic",partitions = {"1"})},
-            containerFactory = "strConsumerFactory" )
+    @StrConsumerCustomListener(groupId = "group-1")
     public void listener2(String message){
         log.info("LISTENER 2 RECEIVED MESSAGE: {}." , message);
     }
 
-    @KafkaListener(groupId = "group-2",topics = "str-topic",containerFactory = "strConsumerFactory" )
+    @StrConsumerCustomListener(groupId = "group-2")
     public void listener3(String message){
         log.info("LISTENER 3 RECEIVED MESSAGE: {}." , message);
     }
